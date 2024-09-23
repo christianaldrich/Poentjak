592d7dadbd3970f7571b2592933114dc30efdeac
christianaldrich <pang.aldrich@gmail.com>
2024-09-24 00:19:14 +0700

    Firebase Configuration

--- Poentjak/PoentjakApp.swift
+++ Poentjak/PoentjakApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct PoentjakApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+//    init(){
+//        FirebaseApp.configure()
+//        print("Configured Firebase!")
+//    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        print("Configured Firebase!")
+        return true
     }
 }

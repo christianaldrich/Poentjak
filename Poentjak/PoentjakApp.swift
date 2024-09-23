07bfa5f16e40a17bd813e5b6365b4d00c718c517
christianaldrich <pang.aldrich@gmail.com>
2024-09-23 18:52:11 +0700

    Initial Commit

---
+++ Poentjak/PoentjakApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoentjakApp.swift
+//  Poentjak
+//
+//  Created by Christian Aldrich Darrien on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PoentjakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

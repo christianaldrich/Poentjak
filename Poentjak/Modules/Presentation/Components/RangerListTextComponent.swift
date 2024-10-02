28a39750b4bb10e4c1d53019f6d7e42f28a5267c
christianaldrich <pang.aldrich@gmail.com>
2024-10-02 22:28:38 +0700

    Active Emergencies FE Progress

--- Poentjak/Modules/Presentation/Components/RangerListTextComponent.swift
+++ Poentjak/Modules/Presentation/Components/RangerListTextComponent.swift
@@ -13,21 +13,71 @@ struct RangerNeedHelpComponent: View {
     var onConfirmRescue: () -> Void
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("ID: \(user.id)")
-            Text("Name: \(user.name)")
-            Text("Age: \(user.age)")
-            Text("Gender: \(user.gender)")
-            Text("Height: \(user.height)")
-            Text("Weight: \(user.weight)")
-            Text("Is in Danger: \(user.isDanger ? "Yes" : "No")")
-            Text("Is in Rescue: \(user.isInRescue ? "Rescuing" : "Yah dicuekin")")
-            Text("Last Seen at: \(user.lastSeen.latitude), \(user.lastSeen.longitude)")
-            
-            Button("Confirm Rescue") {
-                onConfirmRescue()
+        
+        VStack(alignment: .leading, spacing: 15){
+            HStack{
+                Image("LoremJoe")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 83, height: 55)
+                    .clipShape(RoundedRectangle(cornerRadius: 11))
+                VStack(alignment: .leading){
+                    Text("\(user.name)")
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                    Text("Insert battery level, 3 mins ago") //Insert battery level
+                        .font(.caption)
+                        .fontWeight(.light)
+                }
             }
+            
+            HStack(spacing: 20){
+                VStack{
+                    Text("Last Seen").modifier(AttributesInfoModifier())
+                    Text("POS 2").modifier(HikersAttributeInfoModifier()) // Insert last seen WayPoint ?
+                }
+                VStack{
+                    Text("Body Weight").modifier(AttributesInfoModifier())
+                    Text("\(user.weight) kg").modifier(HikersAttributeInfoModifier())
+                }
+                
+                VStack{
+                    Text("Height").modifier(AttributesInfoModifier())
+                    Text("\(user.height) cm").modifier(HikersAttributeInfoModifier())
+                }
+                
+                VStack{
+                    Text("Gender").modifier(AttributesInfoModifier())
+                    Text("\(user.gender)").modifier(HikersAttributeInfoModifier())
+                }
+                
+                VStack{
+                    Text("Age").modifier(AttributesInfoModifier())
+                    Text("\(user.age)").modifier(HikersAttributeInfoModifier())
+                }
+            }
+            
+            HStack(alignment:.bottom){
+                HStack{
+                    Image("HypoSymbol")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 17, height: 19)
+                    Text("Hypothermia") //Insert Emergency Type
+                        .textCase(.uppercase)
+
+                }
+                .background(.blue)
+                
+                Spacer()
+                
+                Button("Rescue"){
+                    onConfirmRescue()
+                }
+                .background(.red)
+            }
+            .foregroundStyle(.white)
         }
-        .padding()
+        
     }
 }

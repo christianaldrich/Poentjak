65a976905f95ccfc7a3b5432f13b0613571a257a
Felicia Himawan <ffeliciah.00@gmail.com>
2024-10-16 09:46:38 +0700

    color and icon extension done

---
+++ Poentjak/Utilities/Extensions/Color+Extensions.swift
@@ -0,0 +1,75 @@
+//
+//  Color+Extensions.swift
+//  Poentjak
+//
+//  Created by Felicia Himawan on 15/10/24.
+//
+
+import SwiftUI
+
+extension Color {
+    // primary
+    static var primaryGreen500: Color = Color(hex: "1B3C28")
+    static let primaryDarkGreen = Color(hex: "#112619")
+    static let primaryDisabledGreen = Color(hex: "#798c81")
+    static let primaryLightGreen = Color(hex: "#cddbc5")
+    
+    // secondary
+    static var secondaryYellow500: Color = Color(hex: "F5C035")
+    static let secondaryYellow = Color(hex: "#f5e5cc")
+    static let secondaryYellowText = Color(hex: "#7a6b00")
+    static let secondaryYellowLabelBackground = Color(hex: "#fff4cc")
+    
+    // error
+    static var errorRed500: Color = Color(hex: "D23C3C")
+    
+    // accent red
+    static let accentRedSos = Color(hex: "#ed3535")
+    static let accentRedSosDisabled = Color(hex: "#ed3535")
+    static let accentRedBgLabel = Color(hex: "#fbecec")
+    static let accentRedEmergencyLabel = Color(hex: "#b63131")
+    
+    // accent blue
+    static let accentBlue = Color(hex: "#4285f4")
+    
+    // accent green
+    static let accentGreen = Color(hex: "#12b76a")
+    static let accentGreenbg = Color(hex: "#f6fef9")
+    static let accentGreenlabel = Color(hex: "#dff5e2")
+    static let accentGreenlabelpositive = Color(hex: "#054f31")
+    static let accentGreenText = Color(hex: "#2e7d32")
+    
+    // neutral white
+    static let neutralWhiteBiancaWhite = Color(hex: "#f9f4ec")
+    static let neutralWhite = Color(hex: "#ffffff")
+    
+    // neutral gray
+    static let neutralGrayOutline = Color(hex: "#47484a")
+    static let neutralGrayTertiaryGray = Color(hex: "#898a8d")
+    static let neutralGrayQuaternaryGray = Color(hex: "#d9d9d9")
+    static let neutralGrayLightGray = Color(hex: "#ececec")
+    static let neutralGrayCoolGray = Color(hex: "#f2f4f7")
+    static let neutralGrayBackground = Color(hex: "#fcfcfd")
+    
+    // neutral black
+    static let neutralBlack = Color(hex: "#323232")
+    
+    // custom label emergency
+    static let customLabelsEmergencyOrange = Color(hex: "#d76a1e")
+    static let customLabelsEmergencyDarkYellow = Color(hex: "#e69a29")
+    static let customLabelsEmergencyYellow = Color(hex: "#f2b33b")
+    static let customLabelsEmergencyRed = Color(hex: "#b63131")
+    
+    // custom label reminder background color
+    static let customLabelsReminderBgRed = Color(hex: "#fecdca")
+    static let customLabelsReminderBgGreen = Color(hex: "#d1fadf")
+    static let customLabelsReminderBgYellow = Color(hex: "#fbffe4")
+    
+    // custom label reminder icon color
+    static let customLabelsReminderIconRed = Color(hex: "#d92d20")
+    static let customLabelsReminderIconGreen = Color(hex: "#32d583")
+    static let customLabelsReminderIconYellow = Color(hex: "#d39f17")
+    
+    // custom label reminder text color
+    static let customLabelsReminderTextRed = Color(hex: "#912018")
+}
---
+++ Poentjak/Utilities/Extensions/Icon+Extensions.swift
@@ -0,0 +1,188 @@
+//
+//  Icon+Extensions.swift
+//  Poentjak
+//
+//  Created by Felicia Himawan on 15/10/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Image {
+    
+    struct AdminIcon {
+        static var call: Image {
+            return Image("Icons/admin/i_admin_call")
+        }
+        
+        static var checkboxChecked: Image {
+            return Image("Icons/admin/i_admin_checkbox_checked")
+        }
+        
+        static var checkboxUnchecked: Image {
+            return Image("Icons/admin/i_admin_checkbox_unchecked")
+        }
+        
+        static var filter: Image {
+            return Image("Icons/admin/i_admin_filter")
+        }
+        
+        static var history: Image {
+            return Image("Icons/admin/i_admin_history")
+        }
+        
+    }
+    
+    struct AlertIcon {
+        static var success: Image {
+            return Image("Icons/alert/i_alert_success")
+        }
+        
+    }
+    
+    struct ButtonIcon {
+        static var close: Image {
+            return Image("Icons/button/i_btn_close")
+        }
+        
+        static var edit: Image {
+            return Image("Icons/button/i_btn_edit")
+        }
+        
+        static var guide: Image {
+            return Image("Icons/button/i_btn_guide")
+        }
+        
+        static var profile: Image {
+            return Image("Icons/button/i_btn_profile")
+        }
+        
+    }
+    
+    struct ExploreIcon{
+        static var mountainBig: Image {
+            return Image("Icons/explore/i_explore_mountain_big")
+        }
+        
+        static var mountainSmall: Image {
+            return Image("Icons/explore/i_explore_mountain_small")
+        }
+        
+        static var track: Image {
+            return Image("Icons/explore/i_explore_track")
+        }
+        
+    }
+    
+    struct GenderIcon{
+        static var female: Image {
+            return Image("Icons/gender/i_gender_female")
+        }
+        
+        static var male: Image {
+            return Image("Icons/gender/i_gender_male")
+        }
+        
+        static var others: Image {
+            return Image("Icons/gender/i_gender_others")
+        }
+        
+    }
+    
+    struct GuideIcon{
+        static var guideChecked: Image {
+            return Image("Icons/guide/i_guide_check_checked")
+        }
+        
+        static var guideUnchecked: Image {
+            return Image("Icons/guide/i_guide_check_unchecked")
+        }
+    }
+    
+    struct HikerDetailIcon{
+        static var note: Image {
+            return Image("Icons/hikerDetail/i_hd_note")
+        }
+    }
+    
+    struct LabelIcon{
+        static var add: Image {
+            return Image("Icons/label/i_lb_add")
+        }
+        
+        static var age: Image {
+            return Image("Icons/label/i_lb_age")
+        }
+        
+        static var clock: Image {
+            return Image("Icons/label/i_lb_clock")
+        }
+        
+        static var height: Image {
+            return Image("Icons/label/i_lb_height")
+        }
+        
+        static var hypothermia: Image {
+            return Image("Icons/label/i_lb_hypothermia")
+        }
+        
+        static var injured: Image {
+            return Image("Icons/label/i_lb_injured")
+        }
+        
+        static var lost: Image {
+            return Image("Icons/label/i_lb_lost")
+        }
+        
+        static var postBig: Image {
+            return Image("Icons/label/i_lb_post_big")
+        }
+        
+        static var postSmall: Image {
+            return Image("Icons/label/i_lb_post_small")
+        }
+        
+        static var tap: Image {
+            return Image("Icons/label/i_lb_tap")
+        }
+        
+        static var weight: Image {
+            return Image("Icons/label/i_lb_weight")
+        }
+    }
+    
+    struct PickerIcon{
+        static var search: Image {
+            return Image("Icons/picker/i_picker_search")
+        }
+    }
+    
+    struct SoundBoardIcon{
+        static var airhorn: Image {
+            return Image("Icons/soundBoard/i_sb_airhorn")
+        }
+        
+        static var alarm: Image {
+            return Image("Icons/soundBoard/i_sb_alarm")
+        }
+        
+        static var sosMorse: Image {
+            return Image("Icons/soundBoard/i_sb_sos_morse")
+        }
+        
+        static var whistle: Image {
+            return Image("Icons/soundBoard/i_sb_whistle")
+        }
+    }
+    
+    struct TextFieldIcon{
+        static var eyeHide: Image {
+            return Image("Icons/textField/i_tf_eye_hide")
+        }
+        
+        static var eyeUnhide: Image {
+            return Image("Icons/textField/i_tf_eye_unhide")
+        }
+    }
+        
+}

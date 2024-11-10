05c785204fbef22e4c5fe7239141bd145dc36cae
christianaldrich <pang.aldrich@gmail.com>
2024-11-10 14:54:56 +0700

    Init profile branch

---
+++ Poentjak/Modules/HikerProfileModule/Presentation/ViewModels/HikerProfileViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HikerProfileViewModel.swift
+//  Poentjak
+//
+//  Created by Christian Aldrich Darrien on 10/11/24.
+//
+
+import Foundation
+
+struct HikerProfileViewModel{
+    
+}
---
+++ Poentjak/Modules/HikerProfileModule/Presentation/Views/HikerProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  HikerProfileView.swift
+//  Poentjak
+//
+//  Created by Christian Aldrich Darrien on 10/11/24.
+//
+
+import SwiftUI
+
+struct HikerProfileView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HikerProfileView()
+}

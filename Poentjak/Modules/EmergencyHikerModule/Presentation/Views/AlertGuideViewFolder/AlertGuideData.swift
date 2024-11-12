c0a11c8a97e001a9a93eb7b1d1c6dd1eee4dcf4c
Felicia Himawan <ffeliciah.00@gmail.com>
2024-11-12 10:15:28 +0700

    added alert guide illustrations.

--- Poentjak/Modules/EmergencyHikerModule/Presentation/Views/AlertGuideViewFolder/AlertGuideData.swift
+++ Poentjak/Modules/EmergencyHikerModule/Presentation/Views/AlertGuideViewFolder/AlertGuideData.swift
@@ -21,15 +21,15 @@ struct AlertGuideData{
         "WARM": [
             1: AlertGuideContentDataModel(image: "AlertGuideData/hipoAlertGuide1", title: "Wrap up", content: "Bundle the person in anything warm and dry. Blankets, jackets, an emergency blanket, even extra clothes. Keep that body heat in!"),
             2: AlertGuideContentDataModel(image: "AlertGuideData/hipoAlertGuide2", title: "Assess", content: "Take a quick look at how they’re doing, are they shivering, confused, or really cold? Check their condition."),
-            3: AlertGuideContentDataModel(image: "dummy", title: "Raise Heat", content: "Slowly warm them up with your body heat, warm drinks (if they’re awake), or heat packs, just go gentle, no sudden heat!"),
-            4: AlertGuideContentDataModel(image: "dummy", title: "Move to Safety", content: "Get them to a warm, safe spot as soon as you can to keep them protected from the cold. If symptoms get worse, alert our rangers!")
+            3: AlertGuideContentDataModel(image: "AlertGuideData/hipoAlertGuide3", title: "Raise Heat", content: "Slowly warm them up with your body heat, warm drinks (if they’re awake), or heat packs, just go gentle, no sudden heat!"),
+            4: AlertGuideContentDataModel(image: "AlertGuideData/hipoAlertGuide4", title: "Move to Safety", content: "Get them to a warm, safe spot as soon as you can to keep them protected from the cold. If symptoms get worse, alert our rangers!")
         ],
         
         "CARE": [
-            1: AlertGuideContentDataModel(image: "dummy", title: "Calm Down", content: "Stay calm, take deep breaths, and help the injured person relax. Panic won’t help, keeping a cool head will."),
-            2: AlertGuideContentDataModel(image: "dummy", title: "Assess the Injury", content: "Check what’s wrong, is it a twisted ankle, a cut from the fall, or are they unable to move? Prioritize the most urgent issue (like bleeding)."),
-            3: AlertGuideContentDataModel(images: ["dummy", "dummy", "dummy"], title: "Respond", content: "For a twisted ankle, stabilize it with a bandage or support. If they're bleeding, apply pressure to stop it. If they can't move, make sure they’re comfortable and avoid moving them further."),
-            4: AlertGuideContentDataModel(image: "dummy", title: "Evaluate the Situation", content: "Decide if you can treat the injury on-site, if you need to call the rangers or get them to a safer location for medical attention.")
+            1: AlertGuideContentDataModel(image: "AlertGuideData/injuryAlertGuide1", title: "Calm Down", content: "Stay calm, take deep breaths, and help the injured person relax. Panic won’t help, keeping a cool head will."),
+            2: AlertGuideContentDataModel(image: "AlertGuideData/injuryAlertGuide2", title: "Assess the Injury", content: "Check what’s wrong, is it a twisted ankle, a cut from the fall, or are they unable to move? Prioritize the most urgent issue (like bleeding)."),
+            3: AlertGuideContentDataModel(images: ["AlertGuideData/injuryAlertGuide3", "AlertGuideData/injuryAlertGuide4", "AlertGuideData/injuryAlertGuide5"], title: "Respond", content: "For a twisted ankle, stabilize it with a bandage or support. If they're bleeding, apply pressure to stop it. If they can't move, make sure they’re comfortable and avoid moving them further."),
+            4: AlertGuideContentDataModel(image: "AlertGuideData/injuryAlertGuide6", title: "Evaluate the Situation", content: "Decide if you can treat the injury on-site, if you need to call the rangers or get them to a safer location for medical attention.")
         ]
         
 //        "CARE": [
